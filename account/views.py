7fb66fc6321dcd9cd19f328d577a9e9d19b840a7
yogi0067 <yogesh.bansal0067@gmail.com>
2019-11-06 22:13:04 +0530

    User Register

---
+++ account/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+def register(request):
+    return render(request,'register.html')
+    username
+    first_name
+    last_name
+    email
+    password
+def login(request):
+    pass
