037749e6ef2dec26251fef0b5ab444844df7ac01
yogi0067 <yogesh.bansal0067@gmail.com>
2019-11-07 00:28:03 +0530

    register user

--- account/views.py
+++ account/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User, auth
 # Create your views here.
 def register(request):
-    return render(request,'register.html')
-    username
-    first_name
-    last_name
-    email
-    password
+    if request.method == 'POST':
+        print('enter')
+        username = request.POST['username']
+        first_name =request.POST['first_name']
+        last_name =request.POST['last_name']
+        email = request.POST['email']
+        password1 =request.POST['password1']
+        password2 =request.POST['password2']
+        user = User.objects.create_user(username=username,email=email,first_name=first_name,password=password1,last_name=last_name)
+        user.save()
+        print('user created')
+        return redirect('/')
+    else:
+        return render(request,'register.html')
 def login(request):
     pass
--- travel/settings.py
+++ travel/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'travelapp.apps.TravelappConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
